471310a50976edf4bc0138bbe14c8b79a6bbf066
didado <idejrdud@gmail.com>
2023-11-23 17:40:49 +0900

    회원 탈퇴 서비스, 테스트 추가

--- src/main/java/com/website/didado/domain/member/service/impl/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.website.didado.domain.member.service.impl;
-
-import com.website.didado.domain.member.dto.MemberParameter;
-import com.website.didado.domain.member.dto.MemberResponse;
-
-public interface MemberService {
-    MemberResponse signUp(MemberParameter memberParameter);
-    MemberResponse findAll();
-}
