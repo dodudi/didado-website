34ba8296791a8b4fdae545ccf19e9f5f0fe2ea2e
didado <idejrdud@gmail.com>
2023-11-28 16:08:47 +0900

    회원 중복 에러 코드 변경(404 -> 409)

--- src/main/java/com/website/didado/domain/member/exception/MemberCode.java
+++ src/main/java/com/website/didado/domain/member/exception/MemberCode.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum MemberCode {
     NOT_FOUND_MEMBER(404, "존재하지 않는 회원입니다."),
-    DUPLICATE_MEMBER(404, "중복 회원 에러입니다.");
+    DUPLICATE_MEMBER(409, "중복 회원 에러입니다.");
 
     private final int status;
     private final String message;
