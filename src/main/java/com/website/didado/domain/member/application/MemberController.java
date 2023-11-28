718ae1b7235171d10477c7f4aeeffc920664fc5b
didado <idejrdud@gmail.com>
2023-11-28 17:40:59 +0900

    회원 가입 @Valid 처리 추가.(ExceptionHandler)

--- src/main/java/com/website/didado/domain/member/application/MemberController.java
+++ src/main/java/com/website/didado/domain/member/application/MemberController.java
@@ -5,9 +5,15 @@ import com.website.didado.domain.member.dto.MemberResponse;
 import com.website.didado.domain.member.exception.DuplicateMemberException;
 import com.website.didado.domain.member.exception.NotFoundMemberException;
 import com.website.didado.domain.member.service.impl.MemberService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +25,7 @@ public class MemberController {
     }
 
     @PostMapping("/members")
-    public ResponseEntity<MemberResponse> resister(@RequestBody MemberParameter memberParameter) {
+    public ResponseEntity<MemberResponse> resister(@Valid @RequestBody MemberParameter memberParameter) {
         return ResponseEntity.ok(memberService.signUp(memberParameter));
     }
 
@@ -41,6 +47,21 @@ public class MemberController {
     @PostMapping("/members/{id}/update")
     public ResponseEntity<MemberResponse> update(@PathVariable Long id, @RequestBody MemberParameter memberParameter) {
         return ResponseEntity.ok(memberService.update(id, memberParameter));
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> throwMethodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> validResult = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            validResult.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        MemberResponse response = new MemberResponse("유효성 검사에 실패했습니다.", e.getStatusCode().value(), validResult);
+        return ResponseEntity
+                .status(e.getStatusCode().value())
+                .body(response);
     }
 
     @ExceptionHandler({DuplicateMemberException.class})
