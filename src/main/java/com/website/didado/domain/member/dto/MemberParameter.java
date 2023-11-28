da4ba4a31110bce463655f260fa971b4d9a8cffb
didado <idejrdud@gmail.com>
2023-11-28 17:34:56 +0900

    MemberParameter Valid Message 추가.

--- src/main/java/com/website/didado/domain/member/dto/MemberParameter.java
+++ src/main/java/com/website/didado/domain/member/dto/MemberParameter.java
@@ -4,10 +4,10 @@ import com.website.didado.domain.member.domain.Member;
 import jakarta.validation.constraints.NotEmpty;
 
 public record MemberParameter(
-        @NotEmpty String username,
-        @NotEmpty String firstEmail,
-        @NotEmpty String lastEmail,
-        @NotEmpty String password) {
+        @NotEmpty(message = "회원 이름을 입력해 주세요.") String username,
+        @NotEmpty(message = "이메일을 입력해 주세요.") String firstEmail,
+        @NotEmpty(message = "이메일을 입력해 주세요.") String lastEmail,
+        @NotEmpty(message = "비밀번호를 입력해 주세요.") String password) {
     public Member toMember() {
         return new Member(username, fullEmail(), password);
     }
