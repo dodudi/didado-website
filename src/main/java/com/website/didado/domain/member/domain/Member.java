9c1db7e2d679d12304a6c5bcf3b44a99e06dab5e
didado <idejrdud@gmail.com>
2023-11-29 12:20:54 +0900

    feat: JPA Audioting 어노테이션 활성화, Member 엔티티에 생성날짜, 수정날짜 Audioting 적용

--- src/main/java/com/website/didado/domain/member/domain/Member.java
+++ src/main/java/com/website/didado/domain/member/domain/Member.java
@@ -1,17 +1,18 @@
 package com.website.didado.domain.member.domain;
 
 import com.website.didado.domain.member.dto.MemberParameter;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,7 +20,11 @@ public class Member {
     private String username;
     private String email;
     private String password;
+
+    @CreatedDate
     private LocalDateTime createDate;
+
+    @LastModifiedDate
     private LocalDateTime updateDate;
 
     protected Member() {
