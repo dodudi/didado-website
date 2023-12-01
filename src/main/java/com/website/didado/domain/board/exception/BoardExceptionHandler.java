20286a408756a8e50a9b6bef472ac8314750ccdc
didado <idejrdud@gmail.com>
2023-12-01 16:52:18 +0900

    feat: 게시판 에러 핸들러 추가. (유효성 검사)

---
+++ src/main/java/com/website/didado/domain/board/exception/BoardExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.website.didado.domain.board.exception;
+
+import com.website.didado.domain.board.api.BoardController;
+import com.website.didado.domain.member.dto.MemberResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice(basePackageClasses = BoardController.class)
+public class BoardExceptionHandler {
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> throwMethodArgumentNotValidHandler(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> validResult = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            validResult.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        MemberResponse response = new MemberResponse("유효성 검사에 실패했습니다.", e.getStatusCode().value(), validResult);
+        return ResponseEntity
+                .status(e.getStatusCode().value())
+                .body(response);
+    }
+}
