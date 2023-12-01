893687ec2b5914ce111ae310ce2bd24d8fc7e296
didado <idejrdud@gmail.com>
2023-12-01 16:53:21 +0900

    feat: 존재하지 않는 게시판 에러 처리 추가.

--- src/main/java/com/website/didado/domain/board/exception/BoardExceptionHandler.java
+++ src/main/java/com/website/didado/domain/board/exception/BoardExceptionHandler.java
@@ -2,6 +2,7 @@ package com.website.didado.domain.board.exception;
 
 import com.website.didado.domain.board.api.BoardController;
 import com.website.didado.domain.member.dto.MemberResponse;
+import com.website.didado.domain.member.exception.NotFoundMemberException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -26,6 +27,14 @@ public class BoardExceptionHandler {
         MemberResponse response = new MemberResponse("유효성 검사에 실패했습니다.", e.getStatusCode().value(), validResult);
         return ResponseEntity
                 .status(e.getStatusCode().value())
+                .body(response);
+    }
+
+    @ExceptionHandler({NotFoundBoardException.class})
+    public ResponseEntity<Object> throwNotFoundMemberHandler(NotFoundBoardException e) {
+        MemberResponse response = new MemberResponse(e.getMessage(), e.getStatus(), e.getData());
+        return ResponseEntity
+                .status(e.getStatus())
                 .body(response);
     }
 }
