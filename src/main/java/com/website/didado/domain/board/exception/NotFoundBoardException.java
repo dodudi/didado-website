35dde59db29801bf163f5e30e8fdad298436add1
didado <idejrdud@gmail.com>
2023-12-04 12:16:23 +0900

    fix: 존재 하지 않는 게시판 오류 클래스 수정(MemberCode -> BoardCode)

--- src/main/java/com/website/didado/domain/board/exception/NotFoundBoardException.java
+++ src/main/java/com/website/didado/domain/board/exception/NotFoundBoardException.java
@@ -1,17 +1,18 @@
 package com.website.didado.domain.board.exception;
 
+import com.website.didado.domain.board.domain.BoardCode;
 import com.website.didado.domain.member.domain.MemberCode;
 import lombok.Getter;
 
 @Getter
-public class NotFoundBoardException extends RuntimeException{
+public class NotFoundBoardException extends RuntimeException {
     private final String message;
     private final int status;
     private Object data;
 
     public NotFoundBoardException() {
-        this.message = MemberCode.DUPLICATE_MEMBER.getMessage();
-        this.status = MemberCode.DUPLICATE_MEMBER.getStatus();
+        this.message = BoardCode.NOT_FOUND_BOARD.getMessage();
+        this.status = BoardCode.NOT_FOUND_BOARD.getStatus();
     }
 
     public NotFoundBoardException(Object data) {
