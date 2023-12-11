8877457d3fa7bd8ae5c7854e6cb575f207c4704d
didado <idejrdud@gmail.com>
2023-12-11 11:36:10 +0900

    feat: username으로 모든 캐릭터 정보 확인 기능 추가.

---
+++ src/main/java/com/website/didado/domain/lostark/api/CharacterController.java
@@ -0,0 +1,20 @@
+package com.website.didado.domain.lostark.api;
+
+import com.website.didado.domain.lostark.application.CharacterServiceImpl;
+import com.website.didado.domain.lostark.dto.character.CharacterResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CharacterController {
+    private final CharacterServiceImpl characterService;
+
+    @GetMapping("/lostark/{username}/characters")
+    public ResponseEntity<CharacterResponse> searches(@PathVariable(value = "username") String username) {
+        return ResponseEntity.ok(characterService.searches(username));
+    }
+}
---
+++ src/main/java/com/website/didado/domain/lostark/dto/character/CharacterResponse.java
@@ -0,0 +1,4 @@
+package com.website.didado.domain.lostark.dto.character;
+
+public record CharacterResponse(String message, int statusCode, Object data) {
+}
