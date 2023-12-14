c040475e6f212c5da4080e0615c4e37a2a0cde93
didado <idejrdud@gmail.com>
2023-12-14 12:05:56 +0900

    feat: lostark-auction 서브 모듈 추가

---
+++ lostark-auction/src/main/java/com/didado/auction/lostark/domain/LostarkProperty.java
@@ -0,0 +1,4 @@
+package com.didado.auction.lostark.domain;
+
+public record LostarkProperty(String url, String apiKey) {
+}
---
+++ lostark-auction/src/main/java/com/didado/auction/lostark/dto/auction/CategoryItem.java
@@ -0,0 +1,14 @@
+package com.didado.auction.lostark.dto.auction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class CategoryItem {
+
+    @JsonProperty(value = "Code")
+    private Integer code;
+
+    @JsonProperty(value = "CodeName")
+    private String codeName;
+}
