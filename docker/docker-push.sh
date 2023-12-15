b4a634e549b05943305ed2a40f21dbf3d8841687
didado <idejrdud@gmail.com>
2023-12-15 17:26:53 +0900

    feat: docker repository에 push 하기 위해 설정, 쉘 스크립트 추가.

---
+++ docker/docker-push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker push didadu/lostark-armory-image:v1.0.0
+docker push didadu/lostark-auction-image:v1.0.0
+docker push didadu/lostark-character-image:v1.0.0
+docker push didadu/lostark-content-image:v1.0.0
--- docker/docker-swarm-build.sh
+++ docker/docker-swarm-build.sh
@@ -13,10 +13,10 @@ cd ..
 ./gradlew clean :lostark-armory:build :lostark-auction:build :lostark-character:build :lostark-content:build
 
 #도커 컴포즈 실행에 필요한 이미지 빌드
-docker image build --tag lostark-armory-image ./lostark-armory
-docker image build --tag lostark-auction-image ./lostark-auction
-docker image build --tag lostark-character-image ./lostark-character
-docker image build --tag lostark-content-image ./lostark-content
+docker image build --tag didadu/lostark-armory-image:v1.0.0 ./lostark-armory
+docker image build --tag didadu/lostark-auction-image:v1.0.0 ./lostark-auction
+docker image build --tag didadu/lostark-character-image:v1.0.0 ./lostark-character
+docker image build --tag didadu/lostark-content-image:v1.0.0 ./lostark-content
 
 #도커 스웜에 필요한 네트워크 생성, 도커 스웜으로 스택 실행
 cd docker
