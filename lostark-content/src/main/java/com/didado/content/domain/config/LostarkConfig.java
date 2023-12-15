309d12f1c3c63d8cff57e758f5d47eae78cc9668
didado <idejrdud@gmail.com>
2023-12-15 16:42:01 +0900

    fix: docker swarm 으로 인한 코드 변경 적용으로 lostark-content 모듈 코드 추가

--- lostark-content/src/main/java/com/didado/content/domain/config/LostarkConfig.java
+++ lostark-content/src/main/java/com/didado/content/domain/config/LostarkConfig.java
@@ -5,7 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Slf4j
 @Configuration
@@ -18,9 +27,31 @@ public class LostarkConfig {
     private String apiKey;
 
     @Bean
-    public LostarkProperty getLostarkProperty(){
+    @Profile("default")
+    public LostarkProperty getLostarkPropertyDefault(){
         log.debug("Lostark Url={}", url);
         log.debug("Lostark apiKey={}", apiKey);
         return new LostarkProperty(url, apiKey);
+    }
+
+    @Bean
+    @Profile("dev")
+    public LostarkProperty getLostarkPropertyDev() throws IOException {
+        log.debug("Lostark Url={}", readLostarkUrl());
+        log.debug("Lostark apiKey={}", readLostarkApiKey());
+        return new LostarkProperty(readLostarkUrl(), readLostarkApiKey());
+    }
+
+    private String readLostarkUrl() throws IOException {
+        return readFileContent(url);
+    }
+
+    private String readLostarkApiKey() throws IOException {
+        return readFileContent(apiKey);
+    }
+
+    private String readFileContent(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readString(path);
     }
 }
