db88eedf0737eea8daf3bab0d4cd5ec81c3f0ea7
didado <idejrdud@gmail.com>
2023-12-15 17:34:32 +0900

    feat: 푸쉬 이전에 수정된 이미지를 빌드하기 위해 쉘 스크립트 수정

--- docker/docker-push.sh
+++ docker/docker-push.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+docker image build --tag didadu/lostark-armory-image:v1.0.0 ./lostark-armory
+docker image build --tag didadu/lostark-auction-image:v1.0.0 ./lostark-auction
+docker image build --tag didadu/lostark-character-image:v1.0.0 ./lostark-character
+docker image build --tag didadu/lostark-content-image:v1.0.0 ./lostark-content
+
 docker push didadu/lostark-armory-image:v1.0.0
 docker push didadu/lostark-auction-image:v1.0.0
 docker push didadu/lostark-character-image:v1.0.0
